2ef0712daab4e3dbe9084f24a3a6b2a14e8ab9db
Đình Minh <bui51122@gmail.com>
2022-02-23 11:36:57 +0700

    Create view XemGioHang

---
+++ TraSuaLamss/Models/XemGioHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TraSuaLamss.Models
+{
+    [Serializable]
+    public class XemGioHang
+    {
+        public string TenSP { get; set; }
+        public string HinhAnh { get; set; }
+        public decimal GiaBan { get; set; }
+        public int Soluong { get; set; }
+        public decimal 
+    }
+}
