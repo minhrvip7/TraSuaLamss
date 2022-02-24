6ac74770f7e6028f99e43550b6b139aa63cf545f
Đình Minh <bui51122@gmail.com>
2022-02-24 16:28:39 +0700

    Update GIOHANG

--- TraSuaLamss/Models/XemGioHang.cs
+++ TraSuaLamss/Models/XemGioHang.cs
@@ -10,8 +10,8 @@ namespace TraSuaLamss.Models
     {
         public string TenSP { get; set; }
         public string HinhAnh { get; set; }
-        public decimal GiaBan { get; set; }
+        public string GiaBan { get; set; }
         public int Soluong { get; set; }
-        public decimal ThanhTien { get { return GiaBan * Soluong; } }
+        public decimal ThanhTien { get { return Convert.ToDecimal(GiaBan) * Soluong; } }
     }
 }
