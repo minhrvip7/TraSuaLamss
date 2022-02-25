8c980f86738566d9a652f173305231cc5be7f2d5
Sang Triệu <trieutrungtansang@gmail.com>
2022-02-25 21:09:19 +0700

    add Models ChiTietSanPham

---
+++ TraSuaLamss/Models/ChiTietSanPham.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TraSuaLamss.Models
+{
+    public class ChiTietSanPham
+    {
+        public string MaSP { get; set; }
+        public string GiaBan { get; set; }
+        public string TenSP { get; set; }
+        public string MoTa { get; set; }
+        public string Anh { get; set; }
+        public string TenLoai { get; set; }
+        public string TenNL { get; set; }
+    }
+}
