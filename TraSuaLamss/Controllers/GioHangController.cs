adaf0e1dbe0c4f77d86d9449b3bf6a6c07afe09a
Đình Minh <bui51122@gmail.com>
2022-03-02 10:22:57 +0700

    GioHang update

--- TraSuaLamss/Controllers/GioHangController.cs
+++ TraSuaLamss/Controllers/GioHangController.cs
@@ -13,40 +13,66 @@ namespace TraSuaLamss.Controllers
     public class GioHangController : Controller
     {
         private TraSuaContext db = new TraSuaContext();
-        public ActionResult Giohang(string KHID)
+        private const string MaKHSession = "MaKHSession";
+        public ActionResult Giohang()
         {
-            KHID = "KH001";
-            var list = (from e in db.GioHang
+            string KHID = "KH001";
+            IList<XemGioHang> list = (from e in db.GioHang
                         join d in db.SanPham
                         on e.MaSP equals d.MaSP
                         where e.MaKH == KHID
                         select new XemGioHang { GH = e, SP = d }).ToList();
+            ViewBag.MaKH = KHID;
+            ViewData["ListGiohang"] = list;
             return View(list);
         }
-        public ActionResult Dathang()
+        public ActionResult Dathang(List<XemGioHang> lists)
         {
             return View();
         }
+
         public ActionResult Add(string MaKhach, string MaHang, int soluong)
         {
-            var list = (from e in db.GioHang select e).ToList();
-            if (list.Exists(e => e.MaSP == MaHang && e.MaKH == MaKhach))
+            GioHang gioHang = (from e in db.GioHang
+                               where (e.MaKH == MaKhach && e.MaSP == MaHang)
+                               select e).FirstOrDefault();
+            if (gioHang == null)
             {
-                foreach (var item in db.GioHang)
-                {
-                    if (item.MaKH == MaKhach && item.MaSP == MaHang)
-                    {
-                        item.Soluong += soluong;
-                    }
-                    db.SaveChanges();
-                }
+                gioHang = new GioHang();
+                gioHang.MaKH = MaKhach;
+                gioHang.MaSP = MaHang;
+                gioHang.Soluong = soluong;
+                db.GioHang.Add(gioHang);
             }
             else
             {
-                GioHang giohangnew = new GioHang { MaKH = MaKhach, MaSP = MaHang, Soluong = soluong };
-                db.GioHang.Add(giohangnew);
-                db.SaveChanges();
+                gioHang.Soluong += soluong;
             }
+            db.SaveChanges();
+            return RedirectToAction("Giohang");
+        }
+        public ActionResult Delete(string MaKhach, string MaHang)
+        {
+            GioHang gioHang = (from e in db.GioHang
+                               where (e.MaKH == MaKhach && e.MaSP == MaHang)
+                               select e).FirstOrDefault();
+            if (gioHang != null)
+            {
+                db.GioHang.Remove(gioHang);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Giohang");
+        }
+        public ActionResult DeleteAll(string MaKhach)
+        {
+            foreach(var item in db.GioHang)
+            {
+                if (item.MaKH == MaKhach)
+                {
+                    db.GioHang.Remove(item);
+                }
+            }
+            db.SaveChanges();
             return RedirectToAction("Giohang");
         }
     }
