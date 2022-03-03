0c21b080506c89c7275a94b137d07f0167dc1d97
sonson1908@gmail.com <sonson1908@gmail.com>
2022-03-03 11:48:48 +0700

    son

--- TraSuaLamss/Models/DangNhapModel.cs
+++ TraSuaLamss/Models/DangNhapModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace TraSuaLamss.Models
 {
     public class DangNhapModel
     {
+        [Key]
+        [Display(Name = "Tên đăng nhập")]
+        [Required(ErrorMessage = "Bạn phải nhập tài khoản")]
+        public string Username { set; get; }
+
+        [Required(ErrorMessage = "Bạn phải nhập mật khẩu")]
+        [Display(Name = "Mật khẩu")]
+        public string Password { set; get; }
     }
 }
