3cd21aaa3f7cfffdbdce12ee026f027109bf6348
sonson1908@gmail.com <sonson1908@gmail.com>
2022-03-09 12:44:56 +0700

    son

--- TraSuaLamss/Controllers/HomeController.cs
+++ TraSuaLamss/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace TraSuaLamss.Controllers
 {
     public class HomeController : Controller
     {
-        TraSuaContext db=new TraSuaContext();
+        TraSuaContext db = new TraSuaContext();
         public ActionResult Index()
         {
             return View();
@@ -22,14 +22,88 @@ namespace TraSuaLamss.Controllers
             return View();
         }
 
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public bool CheckUsername(string username)
+        {
+            if (db.TAIKHOANs.Count(x => x.Username == username) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckEmail(string email)
+        {
+            if (db.KHACHHANGs.Count(x => x.Email == email) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(DangKyModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (CheckUsername(model.Username))
+                {
+                    ModelState.AddModelError("", "Tên đăng nhập đã tồn tại!");
+                }
+                else if (CheckEmail(model.Email))
+                {
+                    ModelState.AddModelError("", "Email đã tồn tại!");
+                }
+                else
+                {
+                    var tk = new TAIKHOAN();
+                    var kh = new KHACHHANG();
+                    tk.Username = model.Username;
+                    tk.Password = model.Password;
+                    tk.HoTen = model.TenKH;
+                    tk.PhanQuyen = "Khách hàng";
+                    db.TAIKHOANs.Add(tk);
+                    db.SaveChanges();
+                    kh.TenKH = model.TenKH;
+                    kh.Username = model.Username;
+                    kh.NgaySinh = model.NgaySinh;
+                    kh.Email = model.Email;
+                    kh.DiaChi = model.DiaChi;
+                    kh.DienThoai = model.DienThoai;
+                    db.KHACHHANGs.Add(kh);
+                    db.SaveChanges();
+                    ViewBag.Success = "Đăng ký thành công!";
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult DangNhap(DangNhapModel model)
         {
             if (ModelState.IsValid)
             {
                 var username = model.Username;
                 var password = model.Password;
-                TAIKHOAN tk = db.TAIKHOAN.SingleOrDefault(n => n.Username == username && n.Password == password);
+                TAIKHOAN tk = db.TAIKHOANs.SingleOrDefault(n => n.Username == username && n.Password == password);
                 if (tk != null)
                 {
                     ViewBag.Success = "Chúc mừng đăng nhập thành công!";
