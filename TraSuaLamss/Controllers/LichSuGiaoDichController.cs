90e6380b43563db93bcc64043d9bfc34b6a70a46
vuthelong <love01656354368@gmail.com>
2022-03-16 13:35:10 +0700

    LichSuGiaoDich

--- TraSuaLamss/Controllers/LichSuGiaoDichController.cs
+++ TraSuaLamss/Controllers/LichSuGiaoDichController.cs
@@ -22,10 +22,14 @@ namespace TraSuaLamss.Controllers
         public ActionResult Details(string id)
         {
             ViewBag.donHang = (from a in db.DonHang where a.MaDH == id select a).FirstOrDefault();
+
+            ViewBag.Ten = (from x in db.KhachHang join m in db.DonHang on x.MaKH equals m.MaKH where m.MaDH == id select x.TenKH).FirstOrDefault();
+
             List<ChiTietDonHang> listSP = (from x in db.ChiTietDonHang where x.MaHD == id select x).ToList();
             var listCTSP = new List<SanPhamTrongDonHang>();
             foreach(var item in listSP)
             {
+                /*var ctdh = new ChiTietDonHang();*/
                 var sanpham=(from x in db.SanPham where x.MaSP == item.MaSP select x).FirstOrDefault();
                 SanPhamTrongDonHang moi = new SanPhamTrongDonHang(){ chiTietDonHang=item,sanPham=sanpham};
                 listCTSP.Add(moi);
