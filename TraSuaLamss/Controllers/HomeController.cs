1cdac6608721046531e49289abb06dd2675304f5
Nguyen Hoang Anh <hoanganhhetre@gmail.com>
2022-03-18 23:03:10 +0700

    AnhNH update model

--- TraSuaLamss/Controllers/HomeController.cs
+++ TraSuaLamss/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,19 +14,193 @@ namespace TraSuaLamss.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Search()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        public bool CheckUsername(string username)
         {
-            ViewBag.Message = "Your contact page.";
-
+            if (db.TaiKhoan.Count(x => x.Username == username) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckEmail(string email)
+        {
+            if (db.KhachHang.Count(x => x.Email == email) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(DangKyModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (CheckUsername(model.Username))
+                {
+                    ModelState.AddModelError("", "Tên đăng nhập đã tồn tại!");
+                }
+                else if (CheckEmail(model.Email))
+                {
+                    ModelState.AddModelError("", "Email đã tồn tại!");
+                }
+                else
+                {
+                    var tk = new TaiKhoan();
+                    var kh = new KhachHang();
+                    tk.Username = model.Username;
+                    tk.Password = model.Password;
+                    tk.HoTen = model.TenKH;
+                    tk.PhanQuyen = "Khách hàng";
+                    db.TaiKhoan.Add(tk);
+                    db.SaveChanges();
+                    kh.TenKH = model.TenKH;
+                    kh.Username = model.Username;
+                    kh.GioiTinh = model.GioiTinh;
+                    kh.NgaySinh = model.NgaySinh;
+                    kh.Email = model.Email;
+                    kh.DiaChi = model.DiaChi;
+                    kh.DienThoai = model.DienThoai;
+                    db.KhachHang.Add(kh);
+                    db.SaveChanges();
+                    return RedirectToAction("DangNhap", "Home");
+                }
+                return View(model);
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+            kh.DienThoai = model.DienThoai;
+            [HttpPost]
+            public ActionResult DangNhap(DangNhapModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    var username = model.Username;
+                    var password = model.Password;
+                    TaiKhoan tk = db.TaiKhoan.SingleOrDefault(n => n.Username == username && n.Password == password);
+                    if (tk != null)
+                    {
+                        Session["TAIKHOAN"] = tk;
+                        Session["PhanQuyen"] = tk.PhanQuyen;
+                        Session["Hoten"] = tk.HoTen;
+                        Session["Username"] = username;
+                        Session["Password"] = password;
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không tồn tại!");
+                        return View(model);
+                    }
+                }
+                return View(model);
+            }
+            public ActionResult DangXuat()
+            {
+                Session.Clear();
+                return RedirectToAction("Index");
+            }
+            public ActionResult DoiMatKhau()
+            {
+                return View();
+            }
+            [HttpPost]
+            public ActionResult DoiMatKhau(DoiMatKhauModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    string username = Session["Username"].ToString();
+                    string password = Session["Password"].ToString();
+                    var tk = db.TaiKhoan.SingleOrDefault(n => n.Username == username);
+                    if (model.Password == password)
+                    {
+                        var pw = model.NewPassword;
+                        tk.Password = model.NewPassword;
+                        db.SaveChanges();
+                        Session["Password"] = pw;
+                        ViewBag.Success = "Mật khẩu đã được đổi thành công!";
+                        return View(model);
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Mật khẩu hiện tại không đúng!";
+                    }
+                }
+                return View(model);
+            }
+            [HttpGet]
+            public ActionResult DoiThongTin(string username)
+            {
+                username = Session["Username"].ToString();
+                if (username == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                KhachHang kh = db.KhachHang.SingleOrDefault(n => n.Username == username);
+                DoiThongTinModel doiThongTin = new DoiThongTinModel();
+                doiThongTin.Username = username;
+                doiThongTin.TenKH = kh.TenKH;
+                doiThongTin.GioiTinh = kh.GioiTinh;
+                doiThongTin.DiaChi = kh.DiaChi;
+                doiThongTin.DienThoai = kh.DienThoai;
+                doiThongTin.NgaySinh = kh.NgaySinh;
+                if (doiThongTin == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(doiThongTin);
+            }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult DoiThongTin([Bind(Include = "TenKH,GioiTinh,DienThoai,DiaChi,NgaySinh")] DoiThongTinModel model)
+            {
+                model.Username = Session["Username"].ToString();
+                /*var kh = db.KHACHHANG.SingleOrDefault(n => n.Username == username);*/
+                TaiKhoan tk = db.TaiKhoan.SingleOrDefault(n => n.Username == model.Username);
+                KhachHang kh = tk.KHACHHANGs.SingleOrDefault();
+                if (ModelState.IsValid)
+                {
+                    tk.HoTen = model.TenKH;
+                    db.SaveChanges();
+                    kh.TenKH = model.TenKH;
+                    kh.GioiTinh = model.GioiTinh;
+                    kh.NgaySinh = model.NgaySinh;
+                    kh.DiaChi = model.DiaChi;
+                    kh.DienThoai = model.DienThoai;
+                    db.SaveChanges();
+                    ViewBag.Success = "Cập nhật thành công!";
+                }
+                return View(model);
+            }
+            ViewBag.Success = "Chúc mừng đăng nhập thành công!";
+            Session["TAIKHOAN"] = tk;
+            return RedirectToAction("Index");
+        }
+                else
+                {
+                    ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không tồn tại!");
+                }
+                return View(model);
+}
+return View(model);
         }
     }
 }
---
+++ TraSuaLamss/Models/DoiMatKhau.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TraSuaLamss.Models
+{
+    public class DoiMatKhauModel
+    {
+        [Key]
+        [Required(ErrorMessage = "Nhập mật khẩu hiện tại!")]
+        [Display(Name = "Mật khẩu hiện tại")]
+        public string Password { set; get; }
+
+        [Required(ErrorMessage = "Nhập mật khẩu mới!")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Độ dài mật khẩu ít nhất 6 ký tự.")]
+        [Display(Name = "Mật khẩu mới")]
+        public string NewPassword { set; get; }
+
+        [Required(ErrorMessage = "Nhập xác nhận mật khẩu!")]
+        [Compare("NewPassword", ErrorMessage = "Xác nhận mật khẩu không đúng.")]
+        [Display(Name = "Xác nhận mật khẩu")]
+        public string ConfirmPassword { set; get; }
+    }
+}
